9edad023170e8d11b8149d279de538a8042e0ddb
jsonlu <a12345678>
2017-07-28 17:42:52 +0800

    first commit

---
+++ xiaoyaoji-web/src/main/java/cn/com/xiaoyaoji/BsApplication.java
@@ -0,0 +1,19 @@
+package cn.com.xiaoyaoji;
+
+/**
+ * Author:JsonLu
+ * DateTime:17/2/6 20:08
+ * Email:jsonlu@qq.com
+ * Desc:
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
